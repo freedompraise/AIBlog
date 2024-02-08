c3ca93507223f4eb04fa82bc4644bf537e3ed216
Praise Freedom Dike <dikepraise119@gmail.com>
2024-02-08 08:59:00 +0100

    Remove font-bold from newsletter title

--- AIBlogFrontend/src/components/Newsletter.jsx
+++ AIBlogFrontend/src/components/Newsletter.jsx
@@ -14,7 +14,7 @@ const Newsletter = () => {
   return (
     <form id="newsletter" className="bg-black mt-16" onSubmit={handleSubmit(onSubmit)}>
       <div className="container mx-auto flex flex-col items-center">
-        <h2 className="text-3xl font-bold text-white my-4 ">Subscribe to our newsletter</h2>
+        <h2 className="text-3xl text-white my-4 ">Subscribe to our newsletter</h2>
         <p className="text-white mb-4">Get the latest news and updates delivered straight to your inbox.</p>
 
         {isSubmitted ? (
