c19ab916df727d218f7631cf9ce720f950e72798
Praise Freedom Dike <dikepraise119@gmail.com>
2024-02-10 11:06:54 +0100

    Add useEffect and useState hooks to fetch and display featured posts

---
+++ AIBlogFrontend/src/services/api.js
@@ -0,0 +1,27 @@
+// api.js
+import axios from "axios";
+
+const postsUrl = "http://localhost:8000/api/posts/";
+
+export const getPosts = async () => {
+  try {
+    const response = await axios.get(postsUrl);
+    console.log(response.data);
+    return response.data; 
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    throw error; 
+  }
+};
+
+    
+export const getPost = async (slug) => {
+  try {
+    const response = await axios.get(`${postsUrl}${slug}`);
+    console.log(response.data);
+    return response.data; 
+  } catch (error) {
+    console.error("Error fetching post:", error);
+    throw error; 
+  }
+};
