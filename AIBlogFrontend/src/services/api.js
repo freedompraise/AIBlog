a3ad12055388c71c78c0cc5bf0ec63839ff36fe0
Praise Freedom Dike <dikepraise119@gmail.com>
2024-02-10 12:02:26 +0100

    Remove console.log statement in getPosts function

--- AIBlogFrontend/src/services/api.js
+++ AIBlogFrontend/src/services/api.js
@@ -6,7 +6,6 @@ const postsUrl = "http://localhost:8000/api/posts/";
 export const getPosts = async () => {
   try {
     const response = await axios.get(postsUrl);
-    console.log(response.data);
     return response.data; 
   } catch (error) {
     console.error("Error fetching posts:", error);
