60a4f40bf68b00ce7425552ba7449cf2cb45fd90
Praise Freedom Dike <dikepraise119@gmail.com>
2024-02-15 22:14:42 +0100

    Refactor NavNewsletter component styling

--- AIBlogFrontend/src/components/NavNewsletter.jsx
+++ AIBlogFrontend/src/components/NavNewsletter.jsx
@@ -12,7 +12,7 @@ const NavNewsletter = () => {
   };
 
   return (
-    <form id="newsletter" className=" border border:bg-gray-900 bg-black mt-8 px-4 " onSubmit={handleSubmit(onSubmit)}>
+    <form id="newsletter" className="bg-black mt-8 px-4 " onSubmit={handleSubmit(onSubmit)}>
       <div className="container mx-auto flex flex-col text-center">
         <h2 className="text-3xl font-sans text-white mt-6 mb-4">Subscribe to our newsletter</h2>
         <p className="text-white mb-4">Get the latest news and updates delivered straight to your inbox.</p>
@@ -32,7 +32,7 @@ const NavNewsletter = () => {
               className="border border-gray-700 rounded-md p-2 w-full mb-4 focus:border-indigo-500"
               required
             />
-            <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 w-full rounded-md mb-4">
+            <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 w-full rounded-md mb-8">
               SUBMIT
             </button>
           </div>
