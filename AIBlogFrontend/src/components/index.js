e9749bfbf765612cd3b396db150dc19b10ec264a
Praise Freedom Dike <dikepraise119@gmail.com>
2024-02-15 21:54:09 +0100

    Add FormattedText components

--- AIBlogFrontend/src/components/index.js
+++ AIBlogFrontend/src/components/index.js
@@ -4,3 +4,4 @@ export {default as FeaturedPosts} from './FeaturedPosts';
 export {default as LatestPosts} from './LatestPosts';
 export {default as FootNewsletter} from './FootNewsletter';
 export {default as Footer} from './Footer';
+export {default as FormattedText} from './FormattedText';
--- AIBlogFrontend/src/pages/PostDetail.jsx
+++ AIBlogFrontend/src/pages/PostDetail.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getPost } from '../services/api';
-import { okaidia } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { Prism as SyntaxHig } from 'react-syntax-highlighter';
+import { FormattedText } from '../components';
 
 const PostDetail = ({ match: { params: { slug } } }) => {
   const [post, setPost] = useState(null);
@@ -31,7 +30,7 @@ const PostDetail = ({ match: { params: { slug } } }) => {
   }
 
   return (
-    <div className="container text-center mx-auto max-w-screen-xl mt-8 px-4">
+    <div className="container text-center text-black bg-white mx-auto max-w-screen-xl mt-8 px-4">
       <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
       <p className="mb-4">
         Posted on: {new Date(post.date_created).toLocaleDateString()} in {post.category}
@@ -39,11 +38,8 @@ const PostDetail = ({ match: { params: { slug } } }) => {
 
       <img src={`https://placeimg.com/640/480/${post.category}`} alt={post.image_alt_text} className="w-full mb-4" />
 
-      <div className="overflow-x-hidden">
-        <SyntaxHig
-        hlighter language="html" style={okaidia} showLineNumbers>
-          {post.content}
-        </SyntaxHig>
+      <div className="max-w-screen-sm px-2 bg-white text-black">
+        <FormattedText text={post.content} />
       </div>
 
 
