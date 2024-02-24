b11ad89f55914cce40956310a9dfea9ddbf0e7d2
Praise Freedom Dike <dikepraise119@gmail.com>
2024-02-24 22:15:59 +0100

    Update image source in Contact component

--- AIBlogFrontend/src/pages/Contact.jsx
+++ AIBlogFrontend/src/pages/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <div className="container mx-auto px-4 bg-white text-black text-center overflow-auto mb-2"
         >
             <h1 className="text-3xl font-bold text-center mt-6 mb-4">Contact Us</h1>
-            <img className="w-full" src="../public/contact.jpeg" alt="Contact Us" />
+            <img className="w-full" src="/contact.jpeg" alt="Contact Us" />
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 <div className="bg-white shadow-lg p-4">
