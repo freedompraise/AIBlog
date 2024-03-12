f79fc783ac3052e1fb28688887909fbbd06366d3
Praise Freedom Dike <dikepraise119@gmail.com>
2024-03-12 12:39:59 +0100

    Update font size in LatestPosts component

--- AIBlogFrontend/src/components/LatestPosts.jsx
+++ AIBlogFrontend/src/components/LatestPosts.jsx
@@ -28,7 +28,7 @@ if (loading) {
   return (
     <div className="flex px-4  flex-col space-y-4 justify-center text-black sm:h-50 lg:h-100 items-center h-64">
       <FontAwesomeIcon className='fa-spin' icon={faSpinner} spin size="3x" />
-      <p className="text-2xl font-mono">"The best things in life are worth waiting for." - <span className='text-blue-700 font-sm'>Abraham Lincoln</span></p>
+      <p className="text-2xl font-mono">"The best things in life are worth waiting for." - <span className='text-blue-700 font-xs'>Abraham Lincoln</span></p>
     </div>
   );
 }
