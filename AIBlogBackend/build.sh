14dc16dda3db2f558e0783ceb9c205c12b1ff810
Praise Freedom Dike <dikepraise119@gmail.com>
2024-05-08 15:06:55 +0100

    Refactor LatestPosts.jsx to use slug instead of category for conditional styling

--- AIBlogBackend/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit  # exit on error
-
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py makemigrations
-python manage.py migrate
