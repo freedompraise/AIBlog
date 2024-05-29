94a1022b653f40f28bd7d29b324e7ec1961fd124
Praise Freedom Dike <dikepraise119@gmail.com>
2024-05-29 20:52:55 +0100

    chore: Update About page content and styling

--- AIBlogFrontend/src/pages/About.jsx
+++ AIBlogFrontend/src/pages/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
         <div className="container mx-auto px-4 text-center bg-white overflow-auto mb-4 text-black"
         >
             <h1 className="text-3xl font-bold mt-6 mb-4">About Elite Global AI</h1>
-            <p className="font-sans">
+            <p >
             Elite Global AI empowers youth by demystifying artificial intelligence (AI) and fostering financial empowerment. Through workshops, mentorship, and resources, we make AI accessible, relatable, and beneficial for personal and professional growth.
 
             Our mission is to equip individuals with the skills and insights needed to navigate the AI landscape confidently. We believe in leveraging AI as a tool for positive change and opportunity, ensuring that every individual can thrive in an AI-driven world.
