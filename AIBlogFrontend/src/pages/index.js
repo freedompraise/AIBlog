4244a7be6c88f4b4eadfeba95f908675a185d055
Praise Freedom Dike <dikepraise119@gmail.com>
2024-06-05 12:28:20 +0100

    chore: Update routing to include Login and AdminDashboard components

--- AIBlogFrontend/src/pages/index.js
+++ AIBlogFrontend/src/pages/index.js
@@ -1,7 +1,7 @@
-export {default as Home} from './Home';
-export {default as PostDetail} from './PostDetail';
-export {default as About} from './About';
-export {default as Articles} from './Articles';
-export {default as Contact} from './Contact';
-// export {default as Login} from './Login';
-export {default as AdminDashboard} from './AdminDashboard'
+export { default as Home } from "./Home";
+export { default as PostDetail } from "./PostDetail";
+export { default as About } from "./About";
+export { default as Articles } from "./Articles";
+export { default as Contact } from "./Contact";
+export { default as Login } from "./Login";
+export { default as AdminDashboard } from "./AdminDashboard";
