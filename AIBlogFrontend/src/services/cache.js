858d5f5ee58674a8df95aeb4a3fd7b48f215e1eb
Praise Freedom Dike <dikepraise119@gmail.com>
2024-06-06 10:56:12 +0100

    refactor: Update cache.js to clear localStorage and set token after cache expiration

--- AIBlogFrontend/src/services/cache.js
+++ AIBlogFrontend/src/services/cache.js
@@ -1,10 +1,13 @@
 const clearLocalStorage = () => {
-    const timestamp = localStorage.getItem("timestamp");
-    const now = new Date().getTime();
-    const timeDifference = (now - timestamp ) / 60000;
-    if (timeDifference > 120 ) { // set cache time to 2 hours
-        localStorage.clear();
-    }
-}
+  const timestamp = localStorage.getItem("timestamp");
+  const now = new Date().getTime();
+  const timeDifference = (now - timestamp) / 60000;
+  if (timeDifference > 120) {
+    // set cache time to 2 hours
+    const token = localStorage.getItem("token");
+    localStorage.clear();
+    localStorage.setItem("token", token);
+  }
+};
 
 export default clearLocalStorage;
