7579841879edbd5e3aa99115575bc2f3e34a89b2
Praise Freedom Dike <dikepraise119@gmail.com>
2024-06-06 21:42:42 +0100

    refactor: Update CreatePost and EditPost components to use PostForm for form rendering

--- AIBlogFrontend/src/pages/AdminDashboard/CreatePost/index.jsx
+++ AIBlogFrontend/src/pages/AdminDashboard/CreatePost/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { categories } from "../data";
 import { supabase } from "../../../services/supabaseClient";
+import { PostForm } from "../components/PostForm";
 
 export const CreatePost = () => {
   const [newPost, setNewPost] = useState({
@@ -30,76 +31,16 @@ export const CreatePost = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
-      <div>
-        <label htmlFor="title" className="block font-bold mb-2">
-          Title
-        </label>
-        <input
-          type="text"
-          id="title"
-          value={newPost.title}
-          onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
-          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
-      <div>
-        <label htmlFor="snippet" className="block font-bold mb-2">
-          Snippet
-        </label>
-        <textarea
-          id="snippet"
-          value={newPost.snippet}
-          onChange={(e) => setNewPost({ ...newPost, snippet: e.target.value })}
-          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        ></textarea>
-      </div>
-      <div>
-        <label htmlFor="content" className="block font-bold mb-2">
-          Content
-        </label>
-        <textarea
-          id="content"
-          value={newPost.content}
-          onChange={(e) => setNewPost({ ...newPost, content: e.target.value })}
-          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        ></textarea>
-      </div>
-      {/* <div>
-        <label htmlFor="image" className="block font-bold mb-2">
-          Image
-        </label>
-        <input
-          type="file"
-          id="image"
-          onChange={handleImageUpload}
-          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div> */}
-      <div>
-        <label className="block font-bold mb-2">Category</label>
-        <div className="flex flex-wrap gap-2">
-          {categories.map((category) => (
-            <label className="flex items-center" key={category.value}>
-              <input
-                type="radio"
-                value={category.value}
-                checked={newPost.category === category.value}
-                onChange={handleCategoryChange}
-                className="mr-2"
-              />
-              {category.name}
-            </label>
-          ))}
-        </div>
-      </div>
-
-      <button
-        type="submit"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Create Post
-      </button>
-    </form>
+    <>
+      <h2 className="text-2xl font-bold mt-4">Create Post</h2>
+      <PostForm
+        title={newPost.title}
+        snippet={newPost.snippet}
+        content={newPost.content}
+        categories={categories}
+        onChange={(newPost) => setNewPost(newPost)}
+        onSubmit={handleSubmit}
+      />
+    </>
   );
 };
--- AIBlogFrontend/src/pages/AdminDashboard/EditPost/index.jsx
+++ AIBlogFrontend/src/pages/AdminDashboard/EditPost/index.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { supabase } from "../../../services/supabaseClient";
+import { PostForm } from "../components/PostForm";
 
 export const EditPost = ({ setEditPostSlug, slug }) => {
   const [post, setPost] = useState(null);
@@ -18,49 +19,28 @@ export const EditPost = ({ setEditPostSlug, slug }) => {
   }, [slug]);
 
   const handleSaveChanges = async () => {
-    // Update the post in Supabase
     const { error } = await supabase
       .from("Post")
       .update({ title: post.title, snippet: post.snippet })
       .eq("slug", slug);
     if (!error) {
-      setEditPostSlug(null); // Reset the editPostSlug state
+      setEditPostSlug(null);
     }
   };
 
   if (!post) return null;
 
   return (
-    <div>
+    <>
       <h2 className="text-2xl font-bold mt-4">Edit Post</h2>
-      <form>
-        <label className="block font-bold mb-2">Title</label>
-        <input
-          type="text"
-          value={post.title}
-          onChange={(e) => setPost({ ...post, title: e.target.value })}
-          className="block w-full p-2 pl-10 text-gray-700 rounded"
-        />
-        <label className="block font-bold mb-2">Snippet</label>
-        <textarea
-          value={post.snippet}
-          onChange={(e) => setPost({ ...post, snippet: e.target.value })}
-          className="block w-full p-2 pl-10 text-gray-700 rounded"
-        />
-        <label className="block font-bold mb-2">Content</label>
-        <textarea
-          value={post.content}
-          onChange={(e) => setPost({ ...post, content: e.target.value })}
-          className="block w-full p-2 pl-10 text-gray-700 rounded"
-        />
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          type="button"
-          onClick={handleSaveChanges}
-        >
-          Save Changes
-        </button>
-      </form>
-    </div>
+      <PostForm
+        title={post.title}
+        snippet={post.snippet}
+        content={post.content}
+        categories={post.category}
+        onChange={(newPost) => setPost(newPost)}
+        onSubmit={handleSaveChanges}
+      />
+    </>
   );
 };
