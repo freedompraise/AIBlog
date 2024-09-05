f46dab721818206ad622d1d299ef8fb02ae93bff
Praise Freedom Dike <dikepraise119@gmail.com>
2024-09-05 09:49:06 +0100

    chore: Update App component with responsive styling and code formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { Route, Switch} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { Navbar, FootNewsletter, Footer } from "./components/index";
-import { Home, PostDetail, About, Articles, Contact, AdminDashboard, Login } from "./pages/index";
-import "tailwindcss/tailwind.css"
+import {
+  Home,
+  PostDetail,
+  About,
+  Articles,
+  Contact,
+  AdminDashboard,
+  Login,
+} from "./pages/index";
+import "tailwindcss/tailwind.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import clearLocalStorage from "./services/cache";
@@ -10,27 +18,26 @@ import clearLocalStorage from "./services/cache";
 clearLocalStorage();
 
 function App() {
-
   return (
     <div className="bg-white w-screen overflow-x-hidden flex flex-col text-black mx-auto">
-    <SpeedInsights />
-    <Analytics />
-    <Navbar />
-    <div className="px-6 lg:px-20">
-    <Switch> 
-      <Route path="/" exact component={Home} />
-      <Route path="/post/:slug" component={PostDetail} />
-      <Route path="/about" component={About} />
-      <Route path="/articles" component={Articles} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/admin" component={AdminDashboard} />
-      <Route path="/login" component={Login} />
-    </Switch>
-    <FootNewsletter />
+      <SpeedInsights />
+      <Analytics />
+      <Navbar />
+      <div className="px-6 lg:px-56">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/post/:slug" component={PostDetail} />
+          <Route path="/about" component={About} />
+          <Route path="/articles" component={Articles} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/admin" component={AdminDashboard} />
+          <Route path="/login" component={Login} />
+        </Switch>
+        <FootNewsletter />
+      </div>
+      <Footer />
     </div>
-    <Footer />
-    </div>
-  )
+  );
 }
 
 export default App;
