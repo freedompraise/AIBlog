404576963325f54706f32eba1e2b78ffc0a9a9ac
Praise Freedom Dike <dikepraise119@gmail.com>
2024-09-05 09:49:12 +0100

    chore: Update LatestPosts component grid layout for responsiveness

--- src/components/LatestPosts.jsx
+++ src/components/LatestPosts.jsx
@@ -47,7 +47,7 @@ const LatestPosts = () => {
       <div className="rectangle text-left text-white px-4 py-1 bg-black font-mono mt-4">
         LATEST POSTS
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {posts.map((post) => (
           <div
             key={post.slug}
