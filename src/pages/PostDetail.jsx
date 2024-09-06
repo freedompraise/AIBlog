9cf0025c3daac92d7d261e45405ea3dc8a21df10
Praise Freedom Dike <dikepraise119@gmail.com>
2024-09-06 07:44:21 +0100

    chore: Refactor PostDetail component for improved readability and consistency

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,20 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { supabase } from '../services/supabaseClient';
-import { FormattedText } from '../components';
-import { Loader } from '../components';
+import { useState, useEffect } from "react";
+import { supabase } from "../services/supabaseClient";
+import { FormattedText, Loader } from "../components";
+import { Link } from "react-router-dom";
 
-const PostDetail = ({ match: { params: { slug } } }) => {
+const PostDetail = ({
+  match: {
+    params: { slug },
+  },
+}) => {
   const [post, setPost] = useState(null);
   const [loading, setLoading] = useState(true);
- 
-  
+
   useEffect(() => {
     const fetchPost = async () => {
       try {
         const { data, error } = await supabase
-          .from('Post')
-          .select('*')
-          .eq('slug', slug)
+          .from("Post")
+          .select("*")
+          .eq("slug", slug)
           .single();
 
         if (error) {
@@ -25,29 +28,46 @@ const PostDetail = ({ match: { params: { slug } } }) => {
 
         setLoading(false);
       } catch (error) {
-        throw error
+        throw error;
       }
     };
 
     fetchPost();
   }, [slug]);
 
-
   if (loading) {
-    return <>
-            <Loader />
-            </>;
+    return (
+      <>
+        <Loader />
+      </>
+    );
   }
 
   return (
-    <div className="container text-center py-2 text-black bg-white mx-auto px-2 lg:px-36 overflow-auto mb-2">
-      <h1 className="text-5xl font-bold mt-8">{post.title}</h1>
-      <h3 className='text-3xl mt-2 font-semibold mb-4'>{post.snippet}</h3>
+    <div className="py-2 mt-12 text-black bg-white mx-auto px-2 lg:px-48 overflow-auto">
+      <nav className="breadcrumb text-sm font-mono">
+        <Link
+          to="/"
+          className="text-gray-800 hover:text-indigo-600 hover:underline"
+        >
+          Elite Blog
+        </Link>{" "}
+        {">"} <span className="text-gray-800">Posts</span>
+        {" > "}
+        <span className="">{post.title}</span>
+      </nav>
+
+      <h1 className="text-5xl font-bold mt-4">{post.title}</h1>
+      <h3 className="text-3xl mt-2 font-semibold mb-4">{post.snippet}</h3>
       <p className="font-mono">
         Posted on: {new Date(post.created_at).toLocaleDateString()}
       </p>
 
-      <img src={post.image ? post.image : '/default-photo.webp'} alt={post.title} className="w-full my-4 font-sans" />
+      <img
+        src={post.image ? post.image : "/default-photo.webp"}
+        alt={post.title}
+        className="w-full my-4 font-sans"
+      />
 
       <div className="mt-8 bg-white text-left">
         <FormattedText text={post.content} />
